05d0c78bbb9ed6a531565e107e747e16e4d497b7
npole0103 <npole0103@naver.com>
2021-01-20 00:05:51 +0900

    parameter_2

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li><a href="index.php?id=HTML">HTML</a></li>
+        <li><a href="index.php?id=CSS">CSS</a></li>
+        <li><a href="index.php?id=JavaScript">JavaScript</a></li>
+    </ol>
+    <h2>
+        <?php
+            echo $_GET['id'];
+        ?>
+    </h2>
+    Lorem ipsum dolor sit amet consectetur adipisi
+    cing elit. Consequuntur, quo quam a modi fugit, doloremque voluptatib
+    us hic, rem ab tempore tempora molestias excepturi eaque non voluptatu
+    m sit at placeat sequi?
+</body>
+
+</html>
