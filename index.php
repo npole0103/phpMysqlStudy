14e0fc76be35a11908f7d5a5165afb2408b37469
npole0103 <npole0103@naver.com>
2021-01-23 14:55:39 +0900

    functionization

--- index.php
+++ index.php
@@ -1,44 +1,66 @@
+<?php
+function print_title()
+{
+    if (isset($_GET['id']))
+        echo $_GET['id'];
+    else
+        echo "Welcome";
+}
+
+function print_description()
+{
+    if (isset($_GET['id']))
+        echo file_get_contents("data/" . $_GET['id']);
+    else
+        echo "Hello, PHP";
+}
+
+function print_list()
+{
+    $list = scandir('./data');
+
+    $i = 0;
+    while ($i < count($list)) {
+        if ($list[$i] != '.' && $list[$i] != '..')
+            echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
+        $i = $i + 1;
+    }
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>
+        <?php
+            print_title();
+        ?>
+    </title>
 </head>
 
 <body>
     <h1><a href="index.php"> WEB</a></h1>
-    
+
     <ol>
         <?php
-        $list = scandir('./data');
-
-        $i = 0;
-        while ($i < count($list)) {
-            if($list[$i] != '.' && $list[$i] != '..')
-                echo "<li><a href=\"index.php?id=$list[$i]\">$list[$i]</a></li>\n";
-            $i = $i + 1;
-        }
+            print_list();
         ?>
     </ol>
 
     <h2>
         <?php
-        if (isset($_GET['id']))
-            echo $_GET['id'];
-        else
-            echo "Welcome";
+        print_title();
         ?>
     </h2>
 
     <?php
-    if (isset($_GET['id']))
-        echo file_get_contents("data/" . $_GET['id']);
-    else
-        echo "Hello, PHP";
+        print_description();
     ?>
-    
+
 </body>
 
 </html>
