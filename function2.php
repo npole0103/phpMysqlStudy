732317862cab74c9e64045984415a680ff8edcb0
npole0103 <npole0103@naver.com>
2021-01-23 14:41:20 +0900

    function2-2

--- function2.php
+++ function2.php
@@ -18,5 +18,16 @@
         basic();
         basic();
     ?>
+
+    <h2>parameter & argument</h2>
+    <?php
+    function sum($left, $right)
+    {
+        print($left + $right);
+        print("<br>");
+    }
+    sum(2, 4);
+    sum(4, 6);
+    ?>
 </body>
 </html>
