1367ced223c5b0a58ee9c0bd0c481683a530b5da
npole0103 <npole0103@naver.com>
2021-02-06 17:37:49 +0900

    setting

---
+++ index.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WEB</title>
+</head>
+<body>
+    <h1>WEB</h1>
+    <ol>
+        <li>HTML</li>
+    </ol>
+    <H2>Welcome</H2>
+    Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus
+    , consectetur ratione sed veritatis dolore aperiam est reprehend
+    erit repellat! Sit similique omnis eveniet sequ
+    i laborum autem veniam assumenda commodi est possimus.
+</body>
+</html>
